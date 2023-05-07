8388c444decc38f4b743c8e8c2355b36c9312e1c
Tracy <131429059+TracyHT@users.noreply.github.com>
2023-05-07 13:55:21 +0700

    new size draft

--- Diamond Hunter/src/com/neet/DiamondHunter/Main/GamePanel.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/Main/GamePanel.java
@@ -23,10 +23,10 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	// dimensions
 	// HEIGHT is the playing area size
 	// HEIGHT2 includes the bottom window
-	public static final int WIDTH = 180;
-	public static final int HEIGHT = 180;
+	public static final int WIDTH = 128;
+	public static final int HEIGHT = 128;
 	public static final int HEIGHT2 = HEIGHT + 16;
-	public static final int SCALE = 3;
+	public static final int SCALE = 4;
 	
 	// game loop stuff
 	private Thread thread;
--- Diamond Hunter/src/com/neet/DiamondHunter/TileMap/TileMap.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/TileMap/TileMap.java
@@ -107,11 +107,11 @@ public class TileMap {
 			height = numRows * tileSize;
 			
 			xmin = GamePanel.WIDTH - width;
-			xmin = -width +32;
-			xmax = -32;
+			xmin = -width;
+			xmax = 0;
 			ymin = GamePanel.HEIGHT - height;
-			ymin = -height + 32;
-			ymax = -32;
+			ymin = -height;
+			ymax = 0;
 			
 			String delims = "\\s+";
 			for(int row = 0; row < numRows; row++) {
